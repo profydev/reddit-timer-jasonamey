5e7f7a2b180150c9a2f453281d4aadb7655b4660
jasonamey <34172034+jasonamey@users.noreply.github.com>
2022-01-07 10:41:29 -0500

    Create Components for Project (#1)
    * Create Components for Project
    
    * Add normalize.css
    
    * Add global styles
    
    * Install fonts Bitter and Montserrat
    
    * Add routes with routes / and /search
    
    * Adjust styling preferences according to exercise file
    
    * Add changed lock files
    
    * Change to yarn from npm
    
    * Change project set up to mirror example set up
    
    * Delete css import on index.js
    
    * Add GlobalStyle to App
    
    * Add files to public folder
    
    * Change GlobalStyles file woff fonts
    
    * Change manifest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+
+import theme from './theme';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Normalize />
+          <GlobalStyle />
+          <Routes>
+            <Route exact path="/">
+              Home
+            </Route>
+            <Route path="/search">Search</Route>
+            <Route>404 - Not Found</Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
