496332fb0cb206631de479e98fa274f37b1b1001
Jason Amey <jason.amey@gmail.com>
2022-01-07 09:38:59 -0500

    Add files to public folder

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,18 @@
 const theme = {
-  colors: {
-    white: '#FFFFFF',
+  color: {
+    text: '#93918f',
+  },
+  font: {
+    family: {
+      default: '"Montserrat", sans-serif',
+      headline: '"Bitter", serif',
+    },
+    size: {
+      default: '16px',
+    },
+    lineHeight: {
+      default: 1.69,
+    },
   },
 };
 
